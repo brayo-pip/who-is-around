088e885db59049124bf61120f2f10fff5cb64722
brayo <vukubrian@gmail.com>
2022-10-14 13:05:31 +0300

    Return wifi to it's prior state

--- wifi.sh
+++ wifi.sh
@@ -1,3 +1,15 @@
-nmcli radio wifi on
+nmcli -t device status |grep "wifi:unavailable:"
+wifi_was_off=$?
 
-printf "$(date)\n$(nmcli -t -f SSID,BSSID -e no dev wifi)\n\n" >>~/wifi.log
+if (($wifi_was_off))
+then
+    nmcli radio wifi on
+    nmcli -w 1 device wifi rescan
+fi
+
+printf "$(date)\n$(nmcli -t -f SSID,BSSID,BARS -e no dev wifi)\n\n" >>~/wifi.log
+
+if ((!$wifi_was_off)) # Return the wifi to it's prior state
+then 
+    nmcli -w 5 radio wifi off
+fi
